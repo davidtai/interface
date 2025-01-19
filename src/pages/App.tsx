cd39882feba63703389be1f1f6c4a343834377c6
David Tai <david@verus.io>
2025-01-18 19:27:38 -0600

    Switch to browser router

--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -1,5 +1,5 @@
 import React, { Suspense } from 'react'
-import { HashRouter, Route, Switch } from 'react-router-dom'
+import { BrowserRouter, Route, Switch } from 'react-router-dom'
 import styled from 'styled-components'
 import GoogleAnalyticsReporter from '../components/analytics/GoogleAnalyticsReporter'
 import Header from '../components/Header'
@@ -60,7 +60,7 @@ const Marginer = styled.div`
 export default function App() {
   return (
     <Suspense fallback={null}>
-      <HashRouter>
+      <BrowserRouter>
         <Route component={GoogleAnalyticsReporter} />
         <Route component={DarkModeQueryParamReader} />
         <AppWrapper>
@@ -91,7 +91,7 @@ export default function App() {
             <Marginer />
           </BodyWrapper>
         </AppWrapper>
-      </HashRouter>
+      </BrowserRouter>
     </Suspense>
   )
 }
