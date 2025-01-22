202f6d21327c1c5109fd8f8dc4b76d1f700cb7f1
David Tai <david@verus.io>
2025-01-22 16:42:21 -0600

    fix default list reducer error

--- src/components/Popups/PopupItem.tsx
+++ src/components/Popups/PopupItem.tsx
@@ -5,7 +5,7 @@ import styled, { ThemeContext } from 'styled-components'
 import { animated } from 'react-spring'
 import { PopupContent } from '../../state/application/actions'
 import { useRemovePopup } from '../../state/application/hooks'
-//import ListUpdatePopup from './ListUpdatePopup'
+import ListUpdatePopup from './ListUpdatePopup'
 import TransactionPopup from './TransactionPopup'
 
 export const StyledClose = styled(X)`
@@ -74,12 +74,11 @@ export default function PopupItem({
       txn: { hash, success, summary }
     } = content
     popupContent = <TransactionPopup hash={hash} success={success} summary={summary} />
-  } else if ('listUpdate' in content) { /*
+  } else if ('listUpdate' in content) { 
     const {
       listUpdate: { listUrl, oldList, newList, auto }
     } = content
     popupContent = <ListUpdatePopup popKey={popKey} listUrl={listUrl} oldList={oldList} newList={newList} auto={auto} />
-    */
   }
 
   const faderStyle = useSpring({
